e871128c46615e9ff40e92c6ab40122f2dd5c213
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-02-22 21:44:43 +0100

    Refactor Banner component styles

--- src/components/shared/Banner/Banner.tsx
+++ src/components/shared/Banner/Banner.tsx
@@ -12,12 +12,12 @@ type Props = {
 
 const Banner = ({ heading, text, data }: Props) => {
   return (
-    <div className="grid grid-cols-2">
+    <div className="grid md:grid-cols-2  h-[85vh] ">
       <div
-        className="bg-white dark:bg-[#191919] h-[85vh] w-full flex flex-col justify-center"
+        className="bg-white  md:h-[85vh] w-full flex flex-col justify-center"
         id="search"
       >
-        <div className="grid gap-y-4  mt-20">
+        <div className="grid gap-y-4 md:mt-20">
           <h1 className="text-5xl sm:text-7xl md:text-7xl text-black dark:text-neutral-200 font-clash font-[800]">
             {heading.line1}
             <br className="xs:hidden md:block" /> {heading.line2}
@@ -35,8 +35,8 @@ const Banner = ({ heading, text, data }: Props) => {
           </div>
         </div>
       </div>
-      <div className=" h-[85vh] w-full flex justify-center items-center relative">
-        <div className="w-[500px] h-[500px] rounded-lg mt-20 grid place-items-center relative  ">
+      <div className="hidden md:h-[85vh] w-full md:flex justify-center items-center relative">
+        <div className="w-[500px] h-[500px] rounded-lg mt-20 grid place-items-center relative scale-50 md:scale-100  ">
           <div className="w-[400px] h-[400px] flex-shrink-0 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border border-[#646464] border-opacity-10 rounded-lg"></div>
           <div className="w-[400px] h-[400px] flex-shrink-0 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border border-[#646464] hover:border-[#646464] border-opacity-10 rounded-lg rotate-45 "></div>
           <div className="w-[400px] h-[400px] bg-white flex-shrink-0  border border-[#646464] hover:border-black animate-spin border-opacity-10 -rotate-90 rounded-[50%]"></div>
