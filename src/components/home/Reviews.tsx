9c6453c79a709bc829aadd09e3fe009749730bf2
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-02-22 21:44:32 +0100

    Update font size and padding in Reviews component

--- src/components/home/Reviews.tsx
+++ src/components/home/Reviews.tsx
@@ -16,14 +16,14 @@ function ReviewComponent({ reviews }: Props) {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center py-8 mt-8">
       <div className="flex justify-center items-center">
-        <h4 className="text-5xl font-oleo font-medium dark:text-neutral-400 text-black">
+        <h4 className="text-3xl font-[700] dark:text-neutral-400 text-black">
           Our Community.
         </h4>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 mx-auto xl:w-[950px] gap-6 my-4 sm:my-8">
         {reviews.map((review, index) => (
           <div
-            className={`w-full md:max-w-[360px] p-5 sm:p-6 rounded-lg border dark:border-neutral-700 flex-shrink-0 dark:text-white text-black hover:border-black`}
+            className={`w-full md:max-w-[360px] p-6 rounded-lg border dark:border-neutral-700 flex-shrink-0 dark:text-white text-black hover:border-black`}
             key={index}
           >
             <div className="flex md:flex-row lg:flex-col items-center lg:items-start space-x-3 lg:space-x-0">
