248160d43e4828fe6c72197fba5e2a529fdccb3e
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-02-22 21:45:40 +0100

    Refactor Navbar component

--- src/components/shared/Navbar/Navbar.tsx
+++ src/components/shared/Navbar/Navbar.tsx
@@ -86,9 +86,7 @@ function Header({ nav }: Props) {
         <div className={`${headerClasses}`}>
           <Link href="/" className="hidden lg:block" shallow={true}>
             <div className="group relative flex justify-center items-center border-gray-400 hover:border-gray-700 dark:border-gray-600 dark:hover:border-gray-300 rounded-md dark:text-neutral-300 py-2 cursor-pointer">
-              <h3 className="font-[700] text-black text-3xl lg:text-3xl">
-                Blogger
-              </h3>
+              <h3 className="font-[700] text-black text-3xl">Blogger</h3>
               <div className="opacity-0 bg-black dark:bg-neutral-900 dark:text-neutral-400 text-white text-center text-xs font-raleway rounded-lg py-2 absolute z-40 group-hover:opacity-100 top-full -left-4 mt-4 px-4 pointer-events-none">
                 Home
               </div>
@@ -98,7 +96,7 @@ function Header({ nav }: Props) {
             className="lg:hidden"
             onClick={() => dispatch(showMobileSidebar())}
           >
-            <h3 className="font-oleo font-normal text-3xl lg:text-4xl lg:hidden">
+            <h3 className="font-[700] text-3xl lg:text-4xl lg:hidden">
               Blogger
             </h3>
           </button>
@@ -124,9 +122,9 @@ function Header({ nav }: Props) {
           </div> */}
 
           <div>
-            <div className="hidden sm:flex items-center space-x-4 text-black dark:text-neutral-400">
+            <div className="flex items-center space-x-4 text-black dark:text-neutral-400">
               <div className="border-r border-neutral-500 items-center space-x-2 hidden md:flex text-black dark:text-neutral-400">
-                {hasShadow && !nav && (
+                {/* {hasShadow && !nav && (
                   <Icons
                     icon={search}
                     action={() => handleClickScroll("search")}
@@ -139,7 +137,7 @@ function Header({ nav }: Props) {
                       action={() => dispatch(showSearchBox())}
                     />
                   </div>
-                )}
+                )} */}
 
                 {/* <Toggle /> */}
               </div>
