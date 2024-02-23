81fb669c789771f7ae778b369066613296ae4222
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-02-23 19:22:51 +0100

    Add EditBlogModal to ModalProvider

--- src/components/page-wrappers/ModalProvider/ModalProvider.tsx
+++ src/components/page-wrappers/ModalProvider/ModalProvider.tsx
@@ -3,6 +3,7 @@ import { useAppSelector } from "@/hooks/useAppSelector";
 import { AnimatePresence } from "framer-motion";
 import MobileSidebar from "@/components/shared/MobileSidebar/MobileSidebar";
 import SearchModal from "@/components/modals/SearchModal/SearchModal";
+import EditBlogModal from "@/components/modals/EditBlogModal/EditBlogModal";
 
 interface Props {
   children: React.ReactNode;
@@ -11,6 +12,7 @@ interface Props {
 const ModalProvider: React.FC<Props> = ({ children }) => {
   const searchBoxModal = useAppSelector((state) => state.searchBox.show);
   const mobileSidebar = useAppSelector((state) => state.mobileSidebar.show);
+  const EditModal = useAppSelector((state) => state.blog.show);
 
   return (
     <div>
@@ -19,6 +21,9 @@ const ModalProvider: React.FC<Props> = ({ children }) => {
       </div>
       <div>
         <AnimatePresence>{mobileSidebar && <MobileSidebar />}</AnimatePresence>
+      </div>
+      <div>
+        <AnimatePresence>{EditModal && <EditBlogModal />}</AnimatePresence>
       </div>
       <div>{children}</div>
     </div>
