db0a830d578fa7459eed0597eac05868afd540e2
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-02-23 17:05:48 +0100

    Update typings and UI in SearchBox component

--- src/components/shared/search/Search.tsx
+++ src/components/shared/search/Search.tsx
@@ -1,15 +1,15 @@
 "use client";
 import React, { useState } from "react";
 import Link from "next/link";
-import type { Blog } from "@/typings/blog";
+import type { Blog, newBlog } from "@/typings/blog";
 
 type Props = {
-  blogs: Blog[];
+  blogs: newBlog[];
 };
 
 function SearchBox({ blogs }: Props) {
   const [searchQuery, setSearchQuery] = useState("");
-  const [filteredResults, setFilteredResults] = useState<Blog[]>([]);
+  const [filteredResults, setFilteredResults] = useState<newBlog[]>([]);
 
   const handleInputChange = (event: any) => {
     const { value } = event.target;
@@ -18,7 +18,7 @@ function SearchBox({ blogs }: Props) {
   };
 
   const filterResults = (query: any) => {
-    const filtered = blogs.filter((item: Blog) => {
+    const filtered = blogs.filter((item: newBlog) => {
       for (const key in item) {
         if (typeof item[key] === "string") {
           if (item[key].toLowerCase().includes(query.toLowerCase())) {
@@ -28,7 +28,6 @@ function SearchBox({ blogs }: Props) {
       }
       return false;
     });
-    console.log(filtered);
     setFilteredResults(filtered);
   };
 
@@ -43,27 +42,30 @@ function SearchBox({ blogs }: Props) {
             value={searchQuery}
             onChange={handleInputChange}
           />
-          <button className="p-4 bg-black rounded-r-lg text-white dark:text-neutral-400 font-[500] dark:bg-neutral-800">
+          <button className="p-4 px-8 bg-black rounded-r-lg text-white dark:text-neutral-400 font-[500] dark:bg-neutral-800">
             <i className="fas fa-search fa-lg fa-fw"></i>
             Search
           </button>
         </div>
         {searchQuery && (
-          <div className="absolute left-1/2 transform -translate-x-1/2 top-12 mt-5 h-64 w-full sm:w-[430px] md:w-[485px] bg-white rounded-lg z-50 border-gray-400 hover:border-gray-700 dark:border-gray-600 dark:hover:border-gray-300 overflow-y-auto flex flex-col items-center">
+          <div className="absolute left-1/2 transform -translate-x-1/2 top-16 h-64 w-full sm:w-[430px] md:w-[485px] bg-white rounded-lg z-50 border-gray-400 hover:border-gray-700 dark:border-gray-600 dark:hover:border-gray-300 overflow-y-auto flex flex-col items-center">
             {filteredResults.length ? (
               filteredResults.map((item: any) => (
                 <Link
-                  href={item.link}
+                  href={`/blogs/${item._id}`}
                   key={item.title}
+                  className="w-full"
                   onClick={() => setSearchQuery("")}
                 >
                   <div className="w-full border border-gray-300 hover:border-black rounded-md p-2 mb-2">
-                    <p className="text-xs font-normal">{item.episode}</p>
                     <h2 className="font-medium text-base line-clamp-1">
                       {item.title}
                     </h2>
-                    <p className="line-clamp-1 text-sm">{item.description}</p>
-                    <p className="line-clamp-1 text-sm">{item?.author}</p>
+                    <p className="line-clamp-1 text-sm">{item.content}</p>
+                    <p className="mb-0 sm:mb-2 line-clamp-3 text-sm md:text-base">
+                      Author :{" "}
+                      <span className="font-[500]">{`${item?.author?.firstName} ${item?.author?.lastName}`}</span>
+                    </p>
                   </div>
                 </Link>
               ))
