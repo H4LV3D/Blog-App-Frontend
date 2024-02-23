22f8f4af0bfdf2be7a13def5f85d4874a2b0169d
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-02-23 17:07:38 +0100

    Update selectedBlog type and render content using ReactHtmlParser

--- src/pages/blogs/[link]/sections/read.tsx
+++ src/pages/blogs/[link]/sections/read.tsx
@@ -1,47 +1,26 @@
 import React from "react";
-import type { Blog } from "@/typings/blog";
+import type { newBlog } from "@/typings/blog";
+import ReactHtmlParser from "react-html-parser";
 
 type Props = {
-  selectedBlog: Blog;
+  selectedBlog: newBlog;
 };
 
 const Read = ({ selectedBlog }: Props) => {
   return (
     <>
-      {selectedBlog?.article ? (
-        selectedBlog.article.map((item, index) => (
-          <p className=" mt-6 text-justify !leading-[2rem] " key={index}>
-            {item.p.split("**").map((text, index) => {
-              if (index % 2 === 1) {
-                return (
-                  <span className="font-semibold" key={index}>
-                    {text}
-                  </span>
-                );
-              } else {
-                return text;
-              }
-            })}
+      {selectedBlog?.content ? (
+        selectedBlog.content && (
+          <p className=" mt-6 text-justify !leading-[2rem] ">
+            {ReactHtmlParser(selectedBlog.content)}
           </p>
-        ))
+        )
       ) : (
         <p className="font-raleway text-sm md:text-base mt-6 text-justify">
           Fasten your seatbelt, As this episode is going to be yet another
           inspiring and vital piece of information. Stay Tuned!
         </p>
       )}
-
-      <div className="mt-6 flex flex-wrap items-center">
-        {selectedBlog.tags &&
-          selectedBlog.tags.map((tag: any, index: number) => (
-            <button
-              className="py-2 px-4 text-sm md:text-base rounded-full dark:text-neutral-400 bg-[#f7f7f7] dark:bg-neutral-800 mb-2 mr-3"
-              key={index}
-            >
-              {tag}
-            </button>
-          ))}
-      </div>
     </>
   );
 };
