aac95aa1d6fd2ebbc733144cc04303858f571f67
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-02-23 17:07:28 +0100

    Update typings and remove unnecessary code

--- src/pages/blogs/[link]/sections/info.tsx
+++ src/pages/blogs/[link]/sections/info.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import type { Blog } from "@/typings/blog";
+import type { newBlog } from "@/typings/blog";
 
 type Props = {
-  selectedBlog: Blog;
+  selectedBlog: newBlog;
 };
 
 const Info = ({ selectedBlog }: Props) => {
@@ -12,16 +12,11 @@ const Info = ({ selectedBlog }: Props) => {
         <hr className="mt-4 mb-4 dark:border-neutral-800" />
         <div className="flex justify-between pb-2">
           <div className="flex items-center space-x-4 pb-2">
-            <p className="font-sans font-normal text-sm text-neutral-600 flex items-center space-x-3">
-              <i className="fa-solid fa-hands fa-xl fa-fw"></i>
-              <span> 30</span>
-            </p>
+            <p className="font-sans font-normal text-sm text-neutral-600 flex items-center space-x-3"></p>
           </div>
+          <p className="font-raleway font-normal text-sm"></p>
           <p className="font-raleway font-normal text-sm">
-            {selectedBlog?.duration}
-          </p>
-          <p className="font-raleway font-normal text-sm">
-            {selectedBlog?.date}
+            <span className="font-[500]">Author:</span> {selectedBlog?._id}
           </p>
         </div>
         <hr className="mb-3 dark:border-neutral-800" />
