d68fff4ec24f1b62ab36174a888637730d473e29
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-02-23 20:18:27 +0100

    Add Image component to header section

--- src/pages/blogs/[link]/sections/header.tsx
+++ src/pages/blogs/[link]/sections/header.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import type { newBlog } from "@/typings/blog";
 import Link from "next/link";
+import Image from "next/image";
 
 type Props = {
   selectedBlog: newBlog;
@@ -11,13 +12,20 @@ const Header = ({ selectedBlog }: Props) => {
   return (
     <>
       <div className="h-[30vh] flex justify-center border items-center rounded-xl mt-0 mb-8">
-        <img
+        {/* <img
           // src={`/assets/Bust/peep-${Math.floor(
           //   Math.random() * 104
           // ).toString()}.svg`}
-          src={selectedBlog.image}
+          src={selectedBlog?.image}
           className="h-[26rem]"
           alt="An SVG illustration of a person dressed in different clothings"
+        /> */}
+        <Image
+          src={selectedBlog?.image}
+          alt="An SVG illustration of a person dressed in different clothings"
+          width={250}
+          height={400}
+          className="rounded-[50%]"
         />
       </div>
 
