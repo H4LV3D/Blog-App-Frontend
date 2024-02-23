0da1adde88604a219aa5fafd48ccb1538d03703e
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-02-23 17:07:11 +0100

    Refactor blog page component

--- src/pages/blogs/[link]/index.tsx
+++ src/pages/blogs/[link]/index.tsx
@@ -7,28 +7,39 @@ import Buttons from "./sections/buttons";
 import Read from "./sections/read";
 import Author from "./sections/author";
 import { useRouter } from "next/router";
-import { useAppDispatch } from "@/hooks/useAppDispatch";
-import { useAppSelector } from "@/hooks/useAppSelector";
 import ButtonLoader from "@/components/shared/ButtonLoader/ButtonLoader";
 import Subscribe from "@/components/home/Subscribe";
 import Link from "next/link";
+import { useMutation } from "@tanstack/react-query";
+import { getBlog } from "@/utils/requests/blog";
+import { newBlog } from "@/typings/blog";
+import { useAppSelector } from "@/hooks/useAppSelector";
 
-type Props = {
-  params: {
-    link: number;
-  };
-};
-
-const Blog = ({ params }: Props) => {
+type Props = {};
+const Blog = ({}: Props) => {
   const router = useRouter();
-  const link = Number(router.query.link);
-  const { blogs } = pageData;
-  const selectedBlog = blogs[link];
-
+  const link = router.query.link;
   const [loading, setLoading] = React.useState(false);
+  const [selectedBlog, setSelectedBlog] = React.useState<newBlog>();
+  const user = useAppSelector((state) => state.user.data);
+
+  const mutation = useMutation({
+    mutationFn: async () => {
+      const blog = await getBlog(link as string);
+      setSelectedBlog(blog);
+      return blog;
+    },
+    onSuccess: (data) => {
+      console.log(data);
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
 
   React.useEffect(() => {
     if (selectedBlog === undefined || selectedBlog === null) {
+      mutation.mutate();
       setLoading(true);
     } else {
       setLoading(false);
@@ -56,31 +67,34 @@ const Blog = ({ params }: Props) => {
                         Back
                       </button>
                     </Link>
-                    <div className="flex items-center space-x-4 ">
-                      <Link href={`/blogs`}>
-                        <button className="my-2 text-sm text-neutral-500 hover:text-black border hover:border-black h-[2rem] px-6 rounded-[0.25rem] font-[500] ">
-                          Edit
-                        </button>
-                      </Link>
-                      <Link href={`/blogs`}>
-                        <button className="my-2 text-sm text-neutral-500 hover:text-red-500 border hover:border-red-500 h-[2rem] px-6 rounded-[0.25rem] font-[500] ">
-                          Delete
-                        </button>
-                      </Link>
-                    </div>
+                    {selectedBlog?.author?._id === user?.id && (
+                      <div className="flex items-center space-x-4 ">
+                        <Link href={`/blogs`}>
+                          <button className="my-2 text-sm text-neutral-500 hover:text-black border hover:border-black h-[2rem] px-6 rounded-[0.25rem] font-[500] ">
+                            Edit
+                          </button>
+                        </Link>
+                        <Link href={`/blogs`}>
+                          <button className="my-2 text-sm text-neutral-500 hover:text-red-500 border hover:border-red-500 h-[2rem] px-6 rounded-[0.25rem] font-[500] ">
+                            Delete
+                          </button>
+                        </Link>
+                      </div>
+                    )}
                   </div>
 
                   <Header selectedBlog={selectedBlog} />
 
-                  <Info selectedBlog={selectedBlog} />
+                  <hr className="mt-4 mb-4 dark:border-neutral-800" />
+                  {/* <Info selectedBlog={selectedBlog} /> */}
 
                   <Read selectedBlog={selectedBlog} />
 
-                  <Buttons />
+                  {/* <Buttons /> */}
                 </div>
               </div>
 
-              <Author />
+              <Author selectedBlog={selectedBlog} />
 
               <div className="md:container sm:w-[450px] md:w-[650px] lg:w-[700px] mx-auto xs:px-0 px-8 mt-10 pb-6">
                 <Subscribe />
