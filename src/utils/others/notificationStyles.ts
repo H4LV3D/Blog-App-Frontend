8d4ca610b821866f4eeb92b05692a470a8d87031
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-03-01 18:01:20 +0100

    Update notificationStyles.ts

--- src/utils/others/notificationStyles.ts
+++ src/utils/others/notificationStyles.ts
@@ -1,5 +1,4 @@
 export const notificationStyles = {
-  //   backgroundColor: "#181919",
   color: "white",
   borderColor: "rgba(255, 255, 255, 0.08)",
   borderWidth: "1.5px",
@@ -7,8 +6,13 @@ export const notificationStyles = {
 
 export const notificationStyles2 = () => ({
   title: {
-    // color: "white",
-    fontSize: "1rem",
-    fontWeight: "bold",
+    color: "#000",
+    fontSize: "1.125rem",
+    fontWeight: "light",
   },
+  // description: {
+  //   color: "#000",
+  //   fontSize: "1rem",
+  //   fontWeight: "light",
+  // }
 });
