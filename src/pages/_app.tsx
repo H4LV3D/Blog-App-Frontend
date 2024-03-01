88ddf13669e00ff5343c4c682fe63eeb6bbbf407
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-03-01 18:00:42 +0100

    Update font and notification styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,21 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import "@mantine/core/styles.css";
 import "@mantine/tiptap/styles.css";
 import { ThemeProvider } from "@/contexts/themeContext";
 import { Provider } from "react-redux";
 import { store } from "@/store/store";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { MantineProvider } from "@mantine/core";
-// import { Raleway } from "next/font/google";
+import { Raleway } from "next/font/google";
 import localFont from "next/font/local";
 import { Notifications } from "@mantine/notifications";
 
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
 const queryClient = new QueryClient();
+
+const raleway = Raleway({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
 
 const clashDisplay = localFont({
   src: [
@@ -53,21 +54,13 @@ const clashDisplay = localFont({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={clashDisplay.className}>
-      <MantineProvider withCssVariables>
-        <Notifications position="top-left" zIndex={10077} />
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-        />
-
+    <main className={` ${clashDisplay.className} `}>
+      <MantineProvider
+        theme={{
+          fontFamily: 'clash, "Clash Display", sans-serif',
+        }}
+      >
+        <Notifications position="top-right" zIndex={10077} />
         <QueryClientProvider client={queryClient}>
           <Provider store={store}>
             <ThemeProvider>
@@ -75,7 +68,6 @@ export default function App({ Component, pageProps }: AppProps) {
             </ThemeProvider>
           </Provider>
         </QueryClientProvider>
-        <ToastContainer />
       </MantineProvider>
     </main>
   );
