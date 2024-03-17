8f8883f6dba6f62c58b30652aa06587d58c1aacc
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-03-17 05:39:58 +0100

    Refactor SubscribeForm component styling

--- src/components/forms/SubscribeForm/SubscribeForm.tsx
+++ src/components/forms/SubscribeForm/SubscribeForm.tsx
@@ -31,14 +31,16 @@ function ForgotPasswordForm({}: Props) {
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <div className="w-full sm:max-w-lg">
-          <label
-            htmlFor="email"
-            className="mb-1 text-sm text-light text-neutral-500 dark:text-neutral-500 font-raleway"
-          >
-            Email
-          </label>
-          <div className="flex items-center text-black  mb-3">
+        <div className="w-full max-w-[750px] mx-auto ">
+          <div className="text-start pl-4">
+            <label
+              htmlFor="email"
+              className="mb-1 text-sm text-light text-neutral-500 dark:text-neutral-500 font-ralewaytext-start "
+            >
+              Email
+            </label>
+          </div>
+          <div className="flex items-center space-x-6 text-black w-full mb-3">
             <div className="w-full">
               <input
                 id="email"
@@ -48,13 +50,13 @@ function ForgotPasswordForm({}: Props) {
                 required
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                className="w-full font-raleway rounded-s-[0.5rem] py-2.5 px-5 font-light text-neutral-600 text-base focus:text-black focus:border-black dark:text-neutral-400 dark:focus:text-neutral-300 bg-transparent border border-neutral-300 hover:border-neutral-700 dark:border-neutral-800 focus:outline-none dark:focus:border-neutral-700 placeholder-neutral-400"
+                className="w-full font-clash rounded-[0.5rem] py-[0.85rem] px-5 font-[400] text-neutral-600 text-base focus:text-black focus:border-black dark:text-neutral-400 dark:focus:text-neutral-300 bg-transparent border border-neutral-300 hover:border-neutral-700 dark:border-neutral-800 focus:outline-none dark:focus:border-neutral-700 placeholder-neutral-400"
               />
             </div>
             <button
               type="submit"
               disabled={subscribeMutation.isPending}
-              className="bg-black h-[3rem] px-6 text-white font-[500] rounded-e-[0.5rem] flex items-center "
+              className="bg-black h-[3.5rem] px-10 text-white font-[500] rounded-[0.5rem] flex items-center space-x-2 "
             >
               {subscribeMutation.isPending ? (
                 <ButtonLoader />
