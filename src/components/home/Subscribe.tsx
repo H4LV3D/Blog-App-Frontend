d6b9cfe68dfd5869605b68e7a42d30799eb73967
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-03-17 05:40:12 +0100

    Update Subscribe component UI

--- src/components/home/Subscribe.tsx
+++ src/components/home/Subscribe.tsx
@@ -7,17 +7,22 @@ type Props = {};
 function Subscribe({}: Props) {
   return (
     <>
-      <div className=" py-20 w-full">
-        <h4 className="text-4xl md:text-6xl font-[700] dark:text-neutral-400 text-black mb-2">
-          Subscribe
-        </h4>
-        <p className="text-sm sm:text-base text-gray-700  mb-3 md:max-w-lg">
-          Stay updated with the latest Blogger tales, blogs and newsletters by
-          subscribing to our mailing list.
-        </p>
+      <div className="flex justify-center items-center border-t py-20 min-h-[60vh] w-full">
+        <div className="text-center w-full grid place-items-center ">
+          <p className="text-xl dark:text-neutral-400 text-black max-w-md text-center  ">
+            Newsletter
+          </p>
+          <h4 className="text-4xl md:text-5xl font-[700] dark:text-neutral-400 text-black mb-4">
+            Subscribe Today!
+          </h4>
+          <p className="text-sm sm:text-base text-gray-700 font-raleway mb-3 md:max-w-md">
+            Stay updated with the latest Blogger tales, blogs and newsletters by
+            subscribing to our mailing list.
+          </p>
 
-        <div className="sm:w-full sm:max-w-lg">
-          <SubscribeForm />
+          <div className="w-full mt-6">
+            <SubscribeForm />
+          </div>
         </div>
       </div>
     </>
