9f6f07cbb53b02847d01b35bed7de00f76474f5a
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-03-17 05:40:47 +0100

    Refactor home page component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,8 @@
-"use client";
 import React from "react";
-import Navbar from "@/components/shared/Navbar/Navbar";
-import pageData from "@/data/index.json";
-import Generations from "@/components/home/Generations";
-import Collaborative from "@/components/home/Collaborative";
-import Review from "@/components/home/Reviews";
-// import Subscribe from "@/components/home/Subscribe";
-import Footer from "@/components/shared/footer/Footer";
-import Banner from "@/components/shared/Banner/Banner";
 import Head from "next/head";
-import MaxWidthProvider from "@/components/shared/MaxWidthProvider/MaxWidthProvider";
+import HomePageWrapper from "@/components/page-wrappers/HomePageWrapper/HomePageWrapper";
 
-export default function Home({}: {}) {
-  const { blogs, reviews } = pageData;
-
+export default function Home() {
   return (
     <>
       <Head>
@@ -23,28 +12,7 @@ export default function Home({}: {}) {
         <link rel="icon" href="/favicon.png" />
       </Head>
 
-      <div className="bg-white dark:bg-black/90 min-h-screen w-full cursor-black">
-        <Navbar />
-        <MaxWidthProvider>
-          <Banner
-            heading={{
-              line1: "Information",
-              line2: "at your Finger Tips.",
-            }}
-            text="This is where your experiences come to life through the power of words. Explore, share, and connect with others on a platform designed for your unique voice. Write about your experience or read others!"
-            data={blogs}
-          />
-
-          <Generations />
-        </MaxWidthProvider>
-
-        <Collaborative />
-
-        <div className="md:container mx-auto !px-6 lg:px-0 font-raleway ">
-          <Review reviews={reviews} />
-        </div>
-        <Footer />
-      </div>
+      <HomePageWrapper />
     </>
   );
 }
